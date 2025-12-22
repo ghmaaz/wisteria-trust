e4187c03ab2005e5a9376f42224f1f8d0f88c929
ghmaaz <maazpathan132@gmail.com>
2025-12-22 23:08:02 +0530

    Update verification.routes.js

--- wisteria-backend/src/routes/verification.routes.js
+++ wisteria-backend/src/routes/verification.routes.js
@@ -7,15 +7,16 @@ import { protectAdmin } from "../middlewares/adminAuth.js";
 import { getAllVerifications } from "../controllers/verification.controller.js";
 
 const router = express.Router();
+router.get("/admin/verifications", protectAdmin, getAllVerifications);
 
 /**
  * Admin create verification
  */
 router.post("/verification", protectAdmin, createVerification);
-router.get("/admin/verifications", protectAdmin, getAllVerifications);
 /**
  * Public verify
  */
 router.get("/verify/:id", publicVerify);
 
 export default router;
+
